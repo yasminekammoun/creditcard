6bf53ee6d50e47b0f34a7edac30995e04fdc7be1
yasminekammoun <yasminekm5@gmail.com>
2019-12-06 12:05:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import vmcard from './vmcard.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="main-sec" >
+
+      <h1 className="card-name ">Credit card</h1>
+      <img className="card-logo" src={logo} />
+      <div className="card-info">
+        <div className="column">
+          <p className="card-glob-num">1452 1487 3952 8546</p>
+          <p className='label'>MONTH/YEAR</p>
+          <div className="num-label" >
+
+            <p id="num4" className="bold">5478</p>
+
+            <div className="date" >
+
+              <span>Valid<br /> thru  </span>
+              <i class="fas fa-caret-right"></i>
+              <p className="bold"> 06/2027</p>
+
+            </div>
+
+          </div>
+          <p className="bold">Cardholder</p>
+        </div>
+
+        <img className="card-logo2" src={vmcard} />
+      </div>
     </div>
   );
 }
